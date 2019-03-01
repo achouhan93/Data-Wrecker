8d79aa6d2382e21504a84a4e9691ba4cce9c8eed
ashish180813 <ashish.s.chouhan@gmail.com>
2019-03-01 11:46:52 +0100

    Moving from Branch to Master

---
+++ DataWrecker/Patternidentification/src/test/java/com/data/patternidentification/DataPatternIdentificationTests.java
@@ -0,0 +1,16 @@
+package com.data.patternidentification;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class DataPatternIdentificationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
