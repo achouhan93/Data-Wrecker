ffd8b8c3cba559abe72e38ccca4efa949f8c7a16
Rahul Sarode <rahulsarode9@gmail.com>
2019-03-03 17:11:41 +0100

    Column Statistcis Information signature added

---
+++ Project_Source_Code/ColumnStatistics/src/main/java/com/data/columnStatistics/ColumnStatisticsApplication.java
@@ -0,0 +1,13 @@
+package com.data.columnStatistics;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ColumnStatisticsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ColumnStatisticsApplication.class, args);
+	}
+
+}
