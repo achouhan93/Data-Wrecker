7610f4a035b68a896c1d3cdab5b9a22c9a4d30d3
Paneesh <PPrabhur@its.jnj.com>
2019-03-03 21:44:45 +0100

    Paneesh: Adding Date Service

---
+++ Project_Source_Code/DateService/src/main/java/com/data/wrecker/DateService/DateServiceApplication.java
@@ -0,0 +1,17 @@
+package com.data.wrecker.DateService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+@RestController
+public class DateServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DateServiceApplication.class, args);
+	}
+
+}
