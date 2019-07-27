8514b68d0ba52b04bc341de86b9d9124a837c03e
Paneesh <panishvp@gmail.com>
2019-07-27 08:44:20 +0200

    Commit to resolve conflicts

--- Project_Source_Code/IntegerService/src/main/java/com/data/integerService/IntegerServiceApplication.java
+++ Project_Source_Code/IntegerService/src/main/java/com/data/integerService/IntegerServiceApplication.java
@@ -2,11 +2,8 @@ package com.data.integerService;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
-@EnableDiscoveryClient
-
 public class IntegerServiceApplication {
 
 	public static void main(String[] args) {
--- Project_Source_Code/IntegerService/src/main/java/com/data/lntegerdatatype/controller/lntegerDataTypeController.java
+++ Project_Source_Code/IntegerService/src/main/java/com/data/lntegerdatatype/controller/lntegerDataTypeController.java
@@ -1,7 +1,6 @@
 package com.data.lntegerdatatype.controller;
 
 import java.io.IOException;
-import java.util.List;
 import java.util.Map;
 import java.util.Set;
 
