322ac7a6b7125803797143c2bbdf81b66c702bc5
Paneesh <panishvp@gmail.com>
2019-11-23 20:26:52 +0100

    test commit

--- Project_Source_Code/ColumnStatistics/src/main/java/com/data/columnStatistics/service/ColumnStatisticsService.java
+++ Project_Source_Code/ColumnStatistics/src/main/java/com/data/columnStatistics/service/ColumnStatisticsService.java
@@ -3,4 +3,5 @@ package com.data.columnStatistics.service;
 public interface ColumnStatisticsService {
 	
 	public String getColumnStatistics( String fileName,String dateFormat, String booleanTrueValue, String booleanFalseValue);
+	// this is a test commit 
 }
