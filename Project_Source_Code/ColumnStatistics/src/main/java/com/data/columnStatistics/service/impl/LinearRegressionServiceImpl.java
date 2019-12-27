c5eefd1142bb535c1847c51e84137d353657345f
unknown <chaudhari.harshal1193@gmail.com>
2019-12-27 16:56:04 +0100

    multi olumn stats

---
+++ Project_Source_Code/ColumnStatistics/src/main/java/com/data/columnStatistics/service/impl/LinearRegressionServiceImpl.java
@@ -0,0 +1,78 @@
+package com.data.columnStatistics.service.impl;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.transaction.Transactional;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.springframework.stereotype.Service;
+
+@Transactional
+@Service
+public class LinearRegressionServiceImpl {
+
+	   private ArrayList<Float> Xdata;
+	    private ArrayList<Float> YData;
+	    private Float result1;
+	    private Float result2;
+	    	    
+	    public void linearRegessionEvaluator(ArrayList<Float> firstColumnData, ArrayList<Float> secoundColumnData) {
+	    	LinearRegressionServiceImpl linearRegressionClassifier = new LinearRegressionServiceImpl( firstColumnData , secoundColumnData );
+	    	for (int i=0; i<firstColumnData.size();i++)
+	    	{
+	    	Float predictionedValueBasedOnValueProvided = linearRegressionClassifier.predictValue( firstColumnData.get(i), i);
+	    	}
+			
+		}
+
+	    public LinearRegressionServiceImpl (ArrayList xdata, ArrayList YData) {
+	        Xdata = xdata;
+	        this.YData = YData;
+	    }
+
+	    public Float predictValue ( Float inputValue, int cnt ) {
+	        Float X1 = Xdata.get( cnt ) ;
+	        Float Y1 = YData.get( cnt ) ;
+	        Float Xmean = getXMean( Xdata ) ;
+	        Float Ymean = getYMean( YData ) ;
+	        Float lineSlope = getLineSlope( Xmean , Ymean , X1 , Y1 ) ;
+	        Float YIntercept = getYIntercept( Xmean , Ymean , lineSlope ) ;
+	        Float prediction = ( lineSlope * inputValue ) + YIntercept ;
+	        return prediction ;
+	    }
+
+	    public Float getLineSlope (Float Xmean, Float Ymean, Float X1, Float Y1) {
+	        float num1 = X1 - Xmean;
+	        float num2 = Y1 - Ymean;
+	        float denom = (X1 - Xmean) * (X1 - Xmean);
+	        return (num1 * num2) / denom;
+	    }
+
+	    public float getYIntercept (Float Xmean, Float Ymean, Float lineSlope) {
+	        return Ymean - (lineSlope * Xmean);
+	    }
+
+	    public Float getXMean (ArrayList<Float> Xdata) {
+	        result1 = 0.0f ;
+	        for (Integer i = 0; i < Xdata.size(); i++) {
+	            result1 = result1 + Xdata.get(i);
+	        }
+	        return result1;
+	    }
+
+	    public Float getYMean (ArrayList<Float> Ydata) {
+	        result2 = 0.0f ;
+	        for (Integer i = 0; i < Ydata.size(); i++) {
+	            result2 = result2 + Ydata.get(i);
+	        }
+	        return result2;
+	    }
+
+		
+
+	    
+	
+
+}
---
+++ Project_Source_Code/ColumnStatistics/src/main/java/com/data/columnStatistics/service/impl/MultiColumnStatisticsServiceImpl.java
@@ -0,0 +1,106 @@
+package com.data.columnStatistics.service.impl;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.transaction.Transactional;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.data.columnStatistics.model.ColumnStats;
+import com.data.columnStatistics.model.DataProfilerInfo;
+import com.data.columnStatistics.model.DatasetStats;
+import com.data.columnStatistics.model.ProfilingInfoModel;
+import com.data.columnStatistics.repository.ColumnStatsRepo;
+import com.data.columnStatistics.service.MultiColumnStatisticsService;
+import com.mongodb.BasicDBObject;
+import com.mongodb.DB;
+import com.mongodb.DBCollection;
+import com.mongodb.DBCursor;
+import com.mongodb.Mongo;
+
+@Transactional
+@Service
+public class MultiColumnStatisticsServiceImpl implements MultiColumnStatisticsService {
+	@Autowired
+	private ColumnStatsRepo columnStatsRepo;
+	
+	@Autowired
+	private LinearRegressionServiceImpl LinearRegressionServiceImpl;
+	
+	private Mongo mongo;
+	private DB db;
+	ArrayList<Float> firstColumnData = new ArrayList<Float>();
+	ArrayList<Float> secoundColumnData = new ArrayList<Float>();
+
+	@Override
+	public String multiColumnDataEvaluation(String collectionName) throws JSONException {
+		DataProfilerInfo dataProfilerInfo = null;
+
+		com.data.columnStatistics.model.ProfilingInfoModel profilingInfoModel = new ProfilingInfoModel();
+		DatasetStats datasetStats = null;
+		List<DatasetStats> datasetStatsList = null;
+		ColumnStats columnStats = null;
+		List<DataProfilerInfo> dataProfilerInfoList = columnStatsRepo.findAll();
+		for (int i = 0; i < dataProfilerInfoList.size(); i++) {
+			if (dataProfilerInfoList.get(i).getFileName().equals(collectionName)) {
+				dataProfilerInfo = dataProfilerInfoList.get(i);
+				break;
+			}
+		}
+		ArrayList<String> numericaColumns = new ArrayList<String>();
+		System.out.println("dataProfilerInfo" + dataProfilerInfo);
+		for (int i = 0; i < dataProfilerInfo.getDatasetStats().size(); i++) {
+			String dataTypeOfColumn = dataProfilerInfo.getDatasetStats().get(i).getProfilingInfo().getColumnDataType();
+			String columnName = dataProfilerInfo.getDatasetStats().get(i).getColumnName();
+			if (dataTypeOfColumn.equalsIgnoreCase("Integer") || dataTypeOfColumn.equalsIgnoreCase("Decimal")) {
+				numericaColumns.add(columnName);
+			}
+		}
+		System.out.println("numericaColumns" + numericaColumns);
+
+		mongo = new Mongo("localhost", 27017);
+		db = mongo.getDB("ReverseEngineering");
+		DBCollection collection = db.getCollection(collectionName); // giving the collection name
+		DBCursor cursor = collection.find();
+		JSONArray dbList = new JSONArray();
+
+		while (cursor.hasNext()) {
+			String str = cursor.next().toString();
+			try {
+				JSONObject jsnobj = new JSONObject(str);
+				dbList.put(jsnobj);
+			} catch (JSONException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+
+		for (int j = 0; j < numericaColumns.size(); j++) {
+			for (int k = 0; k < numericaColumns.size(); k++) {
+				if (j == k) {
+					for (int i = 0; i < dbList.length(); i++) {
+						String firstColValue = dbList.getJSONObject(0).get(numericaColumns.get(j)).toString();
+						Float firstColValueFloat = Float.parseFloat(firstColValue);
+						firstColumnData.add(firstColValueFloat);
+						String secoundColValue = dbList.getJSONObject(0).get(numericaColumns.get(k)).toString();
+						Float secoundColValueFloat = Float.parseFloat(secoundColValue);
+						secoundColumnData.add(secoundColValueFloat);
+					}
+					LinearRegressionServiceImpl.linearRegessionEvaluator(firstColumnData,secoundColumnData);
+					
+					/*
+					 * for (int i=0; i<dbList.length(); i++) { String colValue =
+					 * dbList.getJSONObject(0).get("Year").toString();
+					 * firstColumnData.add(colValue); }
+					 */
+				}
+			}
+		}
+		return null;
+	}
+}
